374b0ce50a9fed3958b645c67abf64e6a00df786
AyushPandey2412 <ayushpandey5248@gmail.com>
2025-08-23 13:43:55 +0530

    localhost

--- db.js
+++ db.js
@@ -1,30 +1,30 @@
 // // db.js
-// require("dotenv").config();
-// const { Pool } = require("pg");
-
-// const pool = new Pool({
-//   user: process.env.DB_USER,
-//   password: process.env.DB_PASSWORD,
-//   host: process.env.DB_HOST,
-//   port: process.env.DB_PORT,
-//   database: process.env.DB_NAME,
-// });
-
-// module.exports = pool;
-
-
-// db.js
-
-
-
 require("dotenv").config();
 const { Pool } = require("pg");
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false,
-  },
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  database: process.env.DB_NAME,
 });
 
 module.exports = pool;
+
+
+
+
+
+
+// require("dotenv").config();
+// const { Pool } = require("pg");
+
+// const pool = new Pool({
+//   connectionString: process.env.DATABASE_URL,
+//   ssl: {
+//     rejectUnauthorized: false,
+//   },
+// });
+
+// module.exports = pool;
